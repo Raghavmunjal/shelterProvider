8d49d7a6c5320e0eab7bd51c66b9cb23a0a79d15
Raghavmunjal <rghvmunjal@gmail.com>
2020-10-03 09:23:05 +0530

    üploaded

---
+++ mysql-connection.php
@@ -0,0 +1,24 @@
+<?php
+     //databaseserverlocation="localhost"
+     //userid="root"
+     //password=""
+     //databasename="signup"
+
+
+
+    $con=mysqli_connect("localhost","root","","webproject");
+
+
+
+
+     /*$msg=mysqli_connect_error();
+    if($msg==""){
+    echo "Connection made";
+    }
+    else
+    {
+        echo "Sorry";
+    }*/
+
+
+?>
